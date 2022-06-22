956404b5a78d501fbc6d31aedb0b907f784d6d45
Shubhojyoti Ganguly <>
2022-06-22 16:54:13 +0530

    logic to build menu

---
+++ service/catalog/generate_menu.py
@@ -0,0 +1,32 @@
+from ..models import ServiceArea, Provider, Item, Location
+
+
+class GenerateCatalog:
+    def __init__(self):
+        self.__location__ = None
+        self.__providers__ = None
+        self.__catalog__ = None
+
+    def set_location(self, location):
+        self.__location__ = Location.objects.get(name=location)
+
+    def get_providers(self):
+        self.__providers__ = ServiceArea.objects.filter(location=self.__location__).values('provider')
+
+    def get_catalog(self):
+        self.__catalog__ = Item.objects.filter(provider__in = self.__providers__)
+
+    def calculate_surge(self):
+        def surge(item):
+            item['final_price'] = item['price'] * self.__location__.surge
+            return item
+
+        self.__catalog__ = list(map(surge, self.__catalog__.values()))
+
+    def build_catalog(self, location):
+        self.set_location(location)
+        self.get_providers()
+        self.get_catalog()
+        self.calculate_surge()
+        return self.__catalog__
+
