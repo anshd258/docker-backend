229d7050d3bf60575434bcec28de8e5900fc133a
Manish Suthar <73426474+Manishsu1001@users.noreply.github.com>
2022-06-27 23:41:06 +0530

    solving_conflicts

--- service/views/get_menu.py
+++ service/views/get_menu.py
@@ -4,22 +4,13 @@ from service.catalog import GenerateCatalog
 
 
 class GetMenu(View):
-<<<<<<< HEAD
-    def get(self, request):
-        try:
-            request.GET["location"]
-        except:
-            return JsonResponse({"status": "Please Enter a valid location"})
-        catalog = GenerateCatalog()
-        return JsonResponse({"catalog": catalog.build_catalog(request.GET["location"])})
-=======
+
     def get(self, request) -> JsonResponse:
         catalog = GenerateCatalog()
         try:
             return JsonResponse({"catalog": catalog.build_catalog(request.GET['location'])})
         except Exception as e:
             return JsonResponse({"error": str(e)})
->>>>>>> e4ccabad07f38f7685912cde539a6e4997f984e7
 
     def post(self):
         pass
