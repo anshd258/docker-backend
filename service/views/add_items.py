103dd682467392a80b22c556faec9bc643f5470b
Shubhojyoti Ganguly <>
2022-07-07 23:29:37 +0530

    made discount optional

--- service/views/add_items.py
+++ service/views/add_items.py
@@ -10,6 +10,6 @@ class AddItems(View):
         body = json.loads(request.body)
         order = Order.objects.get(pk=body["order"]["id"])
         for item in body["order"]["items"]:
-            order = order.add_item(item["id"], item["final_price"], item.get("quantity"), item.get("discount"), item.get("option"))
+            order = order.add_item(item["id"], item["final_price"], item.get("quantity", 1), item.get("discount", 0.0), item.get("option", None))
         order_dict = OrderSerializer([order], many=True).data[0]
         return JsonResponse({"order": order_dict})
