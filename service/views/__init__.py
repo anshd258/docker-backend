07c9bca5b59e201b824c1935a4a3aa38c79cf2c8
A <amit.jha6700@gmail.com>
2022-07-08 03:37:11 +0530

    missing export

--- service/views/__init__.py
+++ service/views/__init__.py
@@ -3,3 +3,4 @@ from .create_order import CreateOrder
 from .update_order import UpdateOrder
 from .add_items import AddItems
 from .find_orders import FindOrders
+from .update_item import UpdateItem
