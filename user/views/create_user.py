9f3677610a92861e926b8868ec5baf8d7e03519b
Shubhojyoti Ganguly <>
2022-07-09 16:35:33 +0530

    fixed create user

--- user/views/create_user.py
+++ user/views/create_user.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth.hashers import make_password
 from user.models import UserInfo
 from user.get_user import FindUser
+import json
 
 
 class CreateUser(View):
@@ -11,16 +12,16 @@ class CreateUser(View):
         pass
 
     def post(self, request):
+        data = json.loads(request.body)
         user = User.objects.create(
-            username=request.POST["username"],
-            first_name=request.POST["first_name"],
-            last_name=request.POST["last_name"],
-            password=make_password(request.POST["password"]),
-            email=request.POST["email"],
+            username=data["username"],
+            first_name=data["first_name"],
+            last_name=data["last_name"],
+            password=make_password(data["password"]),
+            email=data["email"],
         )
         user_details = UserInfo.objects.create(
             user_id=user.id,
-            contact=request.POST["contact"],
+            contact=data["contact"],
         )
-        A = FindUser(request.POST["contact"])
-        return JsonResponse(A.get_user())
+        return JsonResponse({"status": "success"})
