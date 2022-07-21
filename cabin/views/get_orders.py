6c55874284d8c995d1ae04c2b2f5c9fa7bc75058
Manish Suthar <73426474+Manishsu1001@users.noreply.github.com>
2022-07-22 00:09:49 +0530

    updated_find_orders (#8)

--- cabin/views/get_orders.py
+++ cabin/views/get_orders.py
@@ -1,21 +1,44 @@
 from django.http import JsonResponse
 from django.views import View
-from cabin.models import Reservation
+from cabin.models import Reservation, Location
 from django.contrib.auth.models import User
+from user.models import UserInfo
 from cabin.serializers import ReservationSerializer
+"""
+    This function will accept following endpoints:
+        /cabin/get-booking/?location=<location_name>
+        /cabin/get-booking/?phone=<phone_number>
+        /cabin/get-booking/?email=<email>
+        /cabin/get-booking/?user_id=<user_id>
+"""
 
 
 class GetReservations(View):
     def get(self, request):
-        user = None
         try:
-            user_id = request.GET["user_id"]
-            user = User.objects.filter(id=user_id).first()
+            user = None
+
+            if request.GET.get("user_id"):
+                user_id = request.GET["user_id"]
+                user = User.objects.filter(id=user_id).first()
+
+            elif request.GET.get("email"):
+                email_id = request.GET["email"]
+                user = User.objects.filter(email=email_id).first()
+
+            elif request.GET.get("phone"):
+                phone = request.GET["phone"]
+                user = UserInfo.objects.filter(contact=phone).first().user
+
+            elif request.GET.get("location"):
+                location = Location.objects.filter(name=request.GET["location"]).first()
+                reservations = Reservation.objects.filter(location=location).all()
+                return JsonResponse({"reservations": ReservationSerializer(reservations, many=True).data})
+
             reservations = Reservation.objects.filter(user=user).all()
             return JsonResponse({"reservations": ReservationSerializer(reservations, many=True).data})
+
         except Exception as e:
             print(e)
             return JsonResponse({"status": e})
 
-    def post(self):
-        pass
