0c0732f927b2e98fd6b19fa430da05015dd974d7
Shrikant Bhadgaonkar <shrikantbhadgaonkar@gmail.com>
2023-09-03 13:51:17 +0530

    minor changes to requirements, orders api

--- service/views/update_item.py
+++ service/views/update_item.py
@@ -11,6 +11,7 @@ class UpdateItem(View):
             body = json.loads(request.body)
             order_item = body['order_item']
             order_item_ob = OrderItem.objects.filter(pk=order_item['id'])
+            del order_item['id']
             order_item_ob.update(**order_item)
             order_item_ob[0].save()
             return JsonResponse(OrderItemSerializer(order_item_ob[0]).data)
