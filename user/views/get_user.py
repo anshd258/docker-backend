242ab4c6a93d4364b5f5c4323c63418282acb354
Shrikant Bhadgaonkar <shrikantbhadgaonkar@gmail.com>
2023-10-08 12:22:54 +0530

    fixed get-user

--- user/views/get_user.py
+++ user/views/get_user.py
@@ -4,11 +4,15 @@ from user.get_user import FindUser
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
+from user.models import UserInfo
+from user.serializers import UserInfoSerializer
 class GetUser(APIView):
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]
     def get(self, request):
-        user = FindUser(request.user)
-        return JsonResponse(user.get_user(), safe=False)
+        user = request.user
+        u=UserInfo.objects.get(user=user)
+        userdata=UserInfoSerializer(u).data
+        return JsonResponse({'userdata':userdata})
     def post(self):
         pass
