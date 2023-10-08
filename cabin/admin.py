4f10279b527c0e8caa3717c21cd9dcb3e0a2b94f
Shrikant Bhadgaonkar <shrikantbhadgaonkar@gmail.com>
2023-10-08 16:31:22 +0530

    registered rooms to admin

--- cabin/admin.py
+++ cabin/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib import admin
-from .models import Location,Reservation, PaymentStatus,Package,Property,LocationMetaData,PropertyMetaData,PropertyReview,LocationMetaData
+from .models import *
 # Register your models here.
 
 # Register your models here.
@@ -12,3 +12,4 @@ admin.site.register(Property)
 admin.site.register(LocationMetaData)
 admin.site.register(PropertyMetaData)
 admin.site.register(PropertyReview)
+admin.site.register(Room)
